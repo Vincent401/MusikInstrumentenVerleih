87bea88b21f39698e79986ff5009c626ac5da8f1
leonk <leon.kolb01@gmx.de>
2022-06-26 13:27:11 +0200

    create new branch

--- src/app/Service_Rent/rent.service.ts
+++ src/app/Service_Rent/rent.service.ts
@@ -10,15 +10,6 @@ export class RentService {
     private REST_API_SERVER = "https://localhost:8080";
    
     public sendPostRent(name: String, email: String, telefonnummer: String, straße: String, plz: String, stadt: String, iban: String, instrument: String){ 
-        
-        
-       
-        this.httpClient.post<any>(this.REST_API_SERVER + "/kunden", {name, email, telefonnummer, straße, plz, stadt, iban, instrument}).subscribe(
-            
-        );
-        
-    }     
-
-   
-
+        this.httpClient.post<any>(this.REST_API_SERVER + "/kunden", {name, email, telefonnummer, straße, plz, stadt, iban, instrument}).subscribe();        
+    }  
 }
